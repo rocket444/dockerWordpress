eb564563323925bcd8c3d8ef97f00aa961f1f6ec
Silvan Von Arx <silvan.vonarx@swissonline.ch>
2021-06-01 16:03:57 +0200

    Added gitignore, deploy.sh and changed some things

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+### PAKETIERE LOKAL
+
+# db dump via mysql-docker-instanz erstellen lassen -> var/lib/mysql
+docker exec -ti wp-deploy_db_1 /usr/bin/mysqldump -u wordpress -wordpress -d wordpress > dump.sql
+
+# zip-file mit user-generated data -> /wp-content/uploads
+tar -C ./wp/wp-content/ -cvzf uploads.tar.gz ./uploads
+
+# zip-file mit themes -> /wp-content/themes
+tar -C -/wp/wp-content/ -cvzf themes.tar.gz ./themes
+
+# zip-file mit plugins -> /wp-content/plugins
+tar -C ./wp/wp-content/ -cvzf plugins.tar.gz ./plugins
+
+# wp-config.php -> wp-config.localdev.php - dies stellt nur eine andere Zugriffsmöglichkeit dar, geht auch einfacher
+docker exec -ti wp-deploy_wordpress_1 cat wp-config.php > wp-config.localdev.php
